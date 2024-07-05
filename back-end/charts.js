825c73cfb04be3aa92a81702f2cf6b0a12b5059b
Manupedro <manupedroc2@gmail.com>
2024-07-05 16:33:44 -0300

    [feat] gráfico de horários nos dias uteis

--- back-end/charts.js
+++ back-end/charts.js
@@ -44,38 +44,85 @@ const dadosSubgrupos = async () => {
         quantidades: subgroups.map(subgroup => subgroup[1])
     };
 }
-//Só funciona para dados do tipo: {cidade: "", subgrupo: "solteiro_A", contato: "Lívio"}
-const estatisticaDe = async (atributo) => {
-    const datas = [];
-    const count = {};
+//Dados disponíveis para subgrupos: [nomes, quantidades], [nomes], [quantidades]
+const dadosHorariosDiasUteis = async () => {
     const querySnapshot = await db.collection(TABELA_CADASTROS).get();
+    const totalHoras = 24;
+    const pessoasEmCasa = Array(totalHoras).fill(0);
+    let datasets = [];
+    let quantidadeTotalDeMembros = 0;
+
     querySnapshot.forEach((doc) => {
-        const data = JSON.parse(doc.data().dadosPessoais)[atributo];
-        count[data] = (count[data] || 0) + 1;
+        let dados = doc.data().horariosDiasUteis;
+        dados = JSON.parse(dados)
+        //console.log(dados)
+        quantidadeTotalDeMembros++;
+        for(let i = 0; i < dados.length; i++){
+            for (let hora = 0; hora < totalHoras; hora++) {
+                //console.log(dados)
+                if (dados[i][hora]) {
+                    pessoasEmCasa[hora]++;
+                }
+            }
+        }
+        datasets.push(dados);
     });
-    for (const data in count) {
-        datas.push([data, count[data]]);
+    const horarioSet = []
+    for(let i in datasets){ //cada pesquisado
+        const horariosDoPesquisado = []
+        for(let j in datasets[i]){ //cada morador
+            for(let k in datasets[i][j]){ //cada hora
+                if(datasets[i][j][k]){
+                    horariosDoPesquisado[k] = (horariosDoPesquisado[k] || 0)+ 1;
+                } else{
+                    horariosDoPesquisado[k] = (horariosDoPesquisado[k] || 0) + 0;
+                }
+               // console.log(datasets[i][j][k])
+            }
+            
+        }
+        horarioSet.push(horariosDoPesquisado)
+
     }
-    console.log(datas)
+    console.log("precisa de algum identificador único para o morador", horarioSet)
     return {
-        datas: datas
+        pessoasEmCasa: horarioSet,
+        quantidadeTotalDeMembros: quantidadeTotalDeMembros
+    };
+}
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 156 + 100); // Parte vermelha
+    const g = Math.floor(Math.random() * 156 + 100); // Verde
+    const b = Math.floor(Math.random() * 156 + 100); // Azul
+
+    return {
+        backgroundColor: `rgba(${r}, ${g}, ${b}, ${0.6})`,
+        borderColor: `rgba(${r}, ${g}, ${b}, ${1})`
     };
 }
 
 
 getBtn.addEventListener('click', dadosCidades);
 refreshBtn.addEventListener('click', async () => {
+    //Gráfico de cidades
     new Chart(graficoCidades, {
         type: 'bar',
         data: {
             labels: (await dadosCidades()).names,
             datasets: [{
-                label: 'quantidades',
+                label: 'Quantidade de Pessoas',
                 data: (await dadosCidades()).quantidades,
                 borderWidth: 1
             }]
         },
         options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Gráfico de cidades'
+                }
+            },
             scales: {
                 y: {
                     beginAtZero: true
@@ -86,22 +133,25 @@ refreshBtn.addEventListener('click', async () => {
     new Chart(graficoHorariosDiasUteis, {
         type: 'bar',
         data: {
-            labels: ['January', 'February', 'March', 'April'],
-            datasets: [{
-                label: 'Dataset 1',
-                data: [10, 20, 30, 40],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }, {
-                label: 'Dataset 2',
-                data: [20, 30, 40, 50],
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }]
+            labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+            datasets: (await dadosHorariosDiasUteis()).pessoasEmCasa.map((item, index)=>{
+                const color = randomColor()
+                return {
+                    label: 'Pessoa ' + index,
+                    data: item,
+                    backgroundColor: color.backgroundColor,
+                    borderColor: color.borderColor,
+                    borderWidth: 1
+                }
+            })
         },
         options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Horários em casa durante Dias Úteis'
+                }
+            },
             scales: {
                 x: {
                     stacked: true
@@ -115,6 +165,6 @@ refreshBtn.addEventListener('click', async () => {
 })
 testeBtn.addEventListener('click', async () => {
     //Por exemplo, quero pegar o retorno da função dadosCidades e fazer algo com ele, sem receber undefined
-    const names = (await dadosSubgrupos())
-     console.log(names)
+    const dados = (await dadosHorariosDiasUteis())
+    console.log(dados.pessoasEmCasa)
  });
