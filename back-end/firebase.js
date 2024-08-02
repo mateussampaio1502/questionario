0b0526431fe4f79c2dbb5bf2e13c0667e1e55485
mateussampaio1502 <mateus.trabalho1502@gmail.com>
2024-08-02 18:21:56 -0300

    deu merda

--- back-end/firebase.js
+++ back-end/firebase.js
@@ -1,13 +1,17 @@
-//Credenciais do Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDsCfhADhGawXAKmijOYciQPUxHXHTui6g",
-    authDomain: "formulario1502.firebaseapp.com",
-    projectId: "formulario1502",
-    storageBucket: "formulario1502.appspot.com",
-    messagingSenderId: "585197050148",
-    appId: "1:585197050148:web:c8aafa012b66bdd9353bb1"
+  apiKey: "AIzaSyDsCfhADhGawXAKmijOYciQPUxHXHTui6g",
+  authDomain: "formulario1502.firebaseapp.com",
+  projectId: "formulario1502",
+  storageBucket: "formulario1502.appspot.com",
+  messagingSenderId: "585197050148",
+  appId: "1:585197050148:web:c8aafa012b66bdd9353bb1"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
